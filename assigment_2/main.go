cffc1a851d52f4d60fffe94817a93322cf7b3c4f
Konrad Barrek (Efix) <konrad.trade@gmail.com>
2023-07-16 20:11:35 +0200

    feat(EG): pointers assignment

--- assigment_2/main.go
+++ assigment_2/main.go
@@ -7,8 +7,35 @@ type person struct {
 	lastName  string
 }
 
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (h house) print() {
+	fmt.Printf("%+v", h)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	// This does not work because go is a pass by value language
+	(*pointerToPerson).firstName = newFirstName
+}
+ 
+type house struct {
+    address string
+    number int16
+}
+
+func (housePointer *house) updateHouseNumber(houseNumber int16) {
+    (*housePointer).number = houseNumber;
+}
+
+func (housePointer *house) updateHouseAddress(houseAddress string) {
+    (*housePointer).address = houseAddress;
+}
+
 func main() {
 	var alex person
+    myHouse := house{address: "Bollowstreet", number: 44}
 
 	alex.firstName = "Alex"
 	alex.lastName = "Anderson"
@@ -16,13 +43,8 @@ func main() {
 	alexPointer := &alex
 	alexPointer.updateName("Alexis")
 	alex.print()
-}
 
-func (p person) print() {
-	fmt.Printf("%+v", p)
-}
-
-func (pointerToPerson *person) updateName(newFirstName string) {
-	// This does not work because go is a pass by value language
-	(*pointerToPerson).firstName = newFirstName
+    myHouse.updateHouseNumber(33)
+    myHouse.updateHouseAddress("Verst")
+    myHouse.print()
 }
